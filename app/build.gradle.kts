31f46406c90d80cd942b572551255e7c19a90302
Justin Crowley <justin.crowley@gmail.com>
2026-05-01 23:01:31 -0400

    Add GitHub Actions release workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,11 +46,16 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+        debug {
+            isMinifyEnabled = false
+            applicationIdSuffix = ".debug"
         }
     }
 
